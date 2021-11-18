986e7d98cf5d52c09a7c6f2be74ae4a9549dcd8f
andrewmina <91993105+andrewmina@users.noreply.github.com>
2021-11-18 12:05:49 -0600

    BaccaratInfo Class Done

---
+++ ClientTest/src/main/java/BaccaratInfo.java
@@ -0,0 +1,60 @@
+import java.io.Serializable;
+
+public class BaccaratInfo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private double playerBid = 0;
+	private double bankerBid = 0;
+	private double tieBid = 0;
+	private double totalBid = 0;
+	
+	BaccaratInfo(double pbid, double bbid,  double tbid) {
+		this.playerBid = pbid;
+		this.bankerBid = bbid;
+		this.tieBid = tbid;
+		this.totalBid = pbid + bbid + tbid;
+	}
+	
+	public void setplayerBid(double val) {
+		this.playerBid = val;
+	}
+	
+	public void setbankerBid(double val) {
+		this.tieBid = val;
+	}
+	
+	public void settieBid(double val) {
+		this.tieBid = val;
+	}
+	
+	public double getTotalBid() {
+		return this.totalBid;
+	}
+	
+	public double getplayerBid() {
+		return this.playerBid;
+	}
+	
+	public double getbankerBid() {
+		return this.bankerBid;
+	}
+	
+	public double gettieBid() {
+		return this.tieBid;
+	}
+	
+	public void resetBids() {
+		this.playerBid = 0;
+		this.bankerBid = 0;
+		this.tieBid = 0;
+		this.totalBid = 0;
+	}
+	
+	
+	
+
+}
